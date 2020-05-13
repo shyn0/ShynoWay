4421877e047fdd1590d97e7d1c69512a3fb61ee8
KellyShyno <anon@mail.ru>
2020-05-13 17:56:15 +0300

    first commit

---
+++ data/info/customShell.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e " 💠""\e[1;31mcustomShell\e[0m" "\e[1;36m- программа, которая поможет вам без\e[0m";
+echo -e "\e[1;36m каких-либо усилий кастомизировать свой shell\e[0m";
+echo "";
+echo -e "🌀""\e[1;35mОсобенности:\e[0m";
+echo -e "\e[1;36m🔸6 цветов\n🔸изменение shell'a(bash)\n🔸изменение приветствия(motd)\n🔸возврат default shell\e[0m";
