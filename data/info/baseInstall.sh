8a560d951b436d31ff2c3a58925d1197d013f08a
KellyShyno <anon@mail.ru>
2020-05-15 16:11:23 +0300

    second commit

--- data/info/baseInstall.sh
+++ data/info/baseInstall.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 #KellyShyno
 
-echo -e "\e[1;31mbaseInstall\e[0m";
+echo -e "\e[1;31m baseInstall\e[0m" "\e[1;36mпредставляет собой два\e[0m";
+echo -e "\e[1;36mскрипта для установки основных утилит для:\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;35mtermux\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;35mlinux дистрибутивов\e[0m";
