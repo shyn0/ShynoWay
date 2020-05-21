c2c4e0a60e7d7b99b45c70e837029f9c7848a212
KellyShyno <kellyshyno@bk.ru>
2020-05-21 16:22:48 +0300

    first commit

---
+++ data/info/eng/baseInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31m baseInstall\e[0m" "\e[1;36mis a two scripts to\e[0m";
+echo -e "\e[1;36minstall the basic utilities for:\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;35mtermux\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;35mlinux distros\e[0m";
---
+++ data/info/eng/customShell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31m customShell\e[0m" "\e[1;36mis a program for shell(bash) customization\e[0m";
+echo "";
+echo -e "\e[1;35mFeatures:\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;36m6 colors\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;36mchanging shell(bash)\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;36mchanging welcome(motd)\e[0m";
+echo -n "⋄ ";
+echo -e "\e[1;36mreturning default shell\e[0m";
