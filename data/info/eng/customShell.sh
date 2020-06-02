969a80a09e673d8dcdd089c9a48deecfccdc589c
KellyShyno <kellyshyno@bk.ru>
2020-06-02 16:55:12 +0300

    littleEdit

--- data/info/eng/customShell.sh
+++ data/info/eng/customShell.sh
@@ -3,7 +3,7 @@
 
 echo -e "\e[1;31mcustomShell\e[0m" "\e[1;36mis a program for shell(bash) customization\e[0m";
 echo "";
-echo -e "\e[1;35mFeatures:\e[0m";
+echo -e "\e[1;35mDescription:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
 echo -e "\e[1;36m6 colors\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
