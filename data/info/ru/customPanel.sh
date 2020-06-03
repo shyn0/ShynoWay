31d44ed6454cc90a3f20333a4fc9a28ac24932e4
KellyShyno <kellyshyno@bk.ru>
2020-06-03 15:33:57 +0300

    firstCommit

---
+++ data/info/ru/customPanel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31mcustomPanel\e[0m" "\e[1;36m - программа для кастомизации панели\e[0m";
+echo -e "\e[1;36mв termux. Она сделает ваше времяпрепровождение в\e[0m";
+echo -e "\e[1;36mtermux более комфортным\e[0m";
+echo "";
+echo -e "\e[1;33m Описание:\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;35mDefaultPanel\e[0m" "\e[1;36m- это termux панель по умолчанию\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;35mClassicPanel\e[0m" "\e[1;36m- это классическая termux панель\e[0m";
+echo -e "\e[1;36m(модифицированная)\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;35mCodingPanel\e[0m" "\e[1;36m- это termux панель для более\e[0m";
+echo -e "\e[1;36mудобного coding'a в termux\e[0m";
+
