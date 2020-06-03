f9b09801206f753975f4cc1f04d27a8ffb1b681c
KellyShyno <kellyshyno@bk.ru>
2020-06-03 15:18:39 +0300

    updateEdit

--- data/update/update.sh
+++ data/update/update.sh
@@ -2,25 +2,23 @@
 #KellyShyno
 #ShynoWay
 
-clear
-sleep 0.4;
+clear;
+sleep 0.2;
 echo "";
-echo -e "\e[1;33mUpdating ShynoWay..\e[0m";
+echo -e "\e[1;36mUpdating..\e[0m";
 echo "";
-sleep 0.4;
-mkdir tmp
-cd tmp
+sleep 0.2;
+mkdir tmp;
+cd tmp;
 #reload repository
-git clone https://github.com/KellyShyno/ShynoWay
-cd ShynoWay/
-chmod -R a+rx *
-cp -r * ../../
-cd ../../
-rm -rf tmp
-clear
-sleep 0.4;
+git clone https://github.com/KellyShyno/ShynoWay;
+cd ShynoWay;
+chmod -R a+rx *;
+cp -r * ../../;
+cd ../../;
+rm -rf tmp;
+clear;
+sleep 0.2;
 echo "";
-echo -e "\e[1;32mUpdating complete\e[0m";
-echo "";
-sleep 0.4;
-#break a leg (&_&)
+echo -e "\e[1;35mUpdated..\e[0m";
+sleep 0.7;
