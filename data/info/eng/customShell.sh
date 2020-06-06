deac0f3994e9072bcb1a64d34965f0f5ef677e88
KellyShyno <kellyshyno@bk.ru>
2020-06-06 11:15:58 +0300

    spaceEdit

--- data/info/eng/customShell.sh
+++ data/info/eng/customShell.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #KellyShyno
 
-echo -e "\e[1;31mcustomShell\e[0m" "\e[1;36m- is the program for shell(bash) customization\e[0m";
+echo -e "\e[1;31mcustomShell\e[0m" "\e[1;36m- is the program for shell(bash)\e[0m";
+echo -e "\e[1;36mcustomization\e[0m";
 echo "";
 echo -e "\e[1;35mDescription:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
