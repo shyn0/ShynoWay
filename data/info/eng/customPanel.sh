c05706d7360c83d463c0c9ca2b9b5c331bdb041b
KellyShyno <kellyshyno@bk.ru>
2020-06-06 11:27:31 +0300

    engPanelAdd

---
+++ data/info/eng/customPanel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31mcustomPanel\e[0m" "\e[1;36m- is the program for panel customization\e[0m";
+echo -e "\e[1;36min termux. It will make your pastime in\e[0m";
+echo -e "\e[1;36mtermux more comfortable\e[0m";
+echo "";
+echo -e "\e[1;33m Description:\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;35mDefaultPanel\e[0m" "\e[1;36m- is the default termux panel\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;35mClassicPanel\e[0m" "\e[1;36m- is the classic termux panel\e[0m";
+echo -e "\e[1;36m(modified)\e[0m";
+echo -en "\e[1;31m⋄ \e[0m";
+echo -e "\e[1;4;35mCodingPanel\e[0m" "\e[1;36m- is the custom termux panel for more\e[0m";
+echo -e "\e[1;36mconvenient coding in termux\e[0m";
