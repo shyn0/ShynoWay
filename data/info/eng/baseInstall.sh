e6805590526b9a50993dd57bf1510cf1944ba333
KellyShyno <kellyshyno@bk.ru>
2020-06-06 11:07:36 +0300

    hyphenAdd

--- data/info/eng/baseInstall.sh
+++ data/info/eng/baseInstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #KellyShyno
 
-echo -e "\e[1;31mbaseInstall\e[0m" "\e[1;36mis a two scripts to\e[0m";
+echo -e "\e[1;31mbaseInstall\e[0m" "\e[1;36m- is a two scripts to\e[0m";
 echo -e "\e[1;36minstall the basic utilities for:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
 echo -e "\e[1;35mtermux\e[0m";
