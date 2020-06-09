73e8d101755ec48c6e0321c84353325ca8c335ce
KellyShyno <kellyshyno@bk.ru>
2020-06-09 12:37:08 +0300

    endComMay

---
+++ data/info/eng/passTime.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31mpassTime\e[0m" "\e[1;36m- is the program to disable\e[0m";
+echo -e "\e[1;4;36mshell\e[0m""\e[1;35m (for termux)\e[0m" "\e[1;36mor\e[0m";
+echo -e "\e[1;4;36mdevice\e[0m""\e[1;35m (for linux distros)\e[0m";
+echo -e "\e[1;36mthrough\e[0m \e[1;4;36mthe timeout\e[0m""\e[1;36m, specified by the user\e[0m";
---
+++ data/info/ru/passTime.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#KellyShyno
+
+echo -e "\e[1;31mpassTime\e[0m" "\e[1;36m- программа для отключения\e[0m";
+echo -e "\e[1;4;36mshell'a\e[0m""\e[1;35m (для termux)\e[0m" "\e[1;36mили самого\e[0m";
+echo -e "\e[1;4;36mdevice\e[0m""\e[1;35m (для linux дистрибутивов)\e[0m";
+echo -e "\e[1;36mчерез\e[0m \e[1;4;36mтайм-аут\e[0m""\e[1;36m, задаваемый user'ом\e[0m";
