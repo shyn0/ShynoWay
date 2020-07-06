b114aab1c15f19ba140717cac147f50ae88784f9
KellyShyno <kellyshyno@bk.ru>
2020-07-06 16:35:37 +0300

    release

--- data/info/eng/Faxe.sh
+++ data/info/eng/Faxe.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+echo -e "\e[1;31mFaxe\e[0m" "\e[1;36m- is the program to delete\e[0m";
+echo -e "\e[1;36mdata from your device\e[0m";
+echo "";
+echo -en "\e[1;37mP.s.\e[0m" "\e[1;31mFaxe \e[0m";
+echo -e "\e[1;36mwork as on\e[0m";
+echo -en "\e[1;35mlinux\e[0m";
+echo -e "\e[1;36m, as on\e[0m" "\e[1;35mtermux\e[0m";
--- data/info/ru/Faxe.sh
+++ data/info/ru/Faxe.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+echo -e "\e[1;31mFaxe\e[0m" "\e[1;36m- программа для удаления\e[0m";
+echo -e "\e[1;36mданных с вашего устройства\e[0m";
+echo "";
+echo -en "\e[1;37mP.s.\e[0m" "\e[1;31mFaxe \e[0m";
+echo -e "\e[1;36mработает как на\e[0m";
+echo -en "\e[1;35mlinux\e[0m";
+echo -e "\e[1;36m, так и на\e[0m" "\e[1;35mtermux\e[0m";
