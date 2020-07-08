f71e708504fbb178e164c6c205d3159099585560
KellyShyno <kellyshyno@bk.ru>
2020-07-08 11:46:20 +0300

    endCom

--- data/info/eng/hiddy.sh
+++ data/info/eng/hiddy.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo -en "\e[1;31mhiddy\e[0m" "\e[1;36m- is the program to \e[0m";
+echo -e "\e[1;35mturn off\e[0m" "\e[1;36mand\e[0m";
+echo -en "\e[1;35mturn on\e[0m" "\e[1;36mthe internet by a \e[0m";
+echo -e "\e[1;35mtime-out\e[0m";
--- data/info/ru/hiddy.sh
+++ data/info/ru/hiddy.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+
+echo -en "\e[1;31mhiddy\e[0m" "\e[1;36m- программа для \e[0m";
+echo -e "\e[1;35mотключения\e[0m" "\e[1;36mи\e[0m";
+echo -en "\e[1;35mвключения\e[0m" "\e[1;36mинтернета по \e[0m";
+echo -e "\e[1;35mtime-out'у\e[0m";
