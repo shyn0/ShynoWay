ea5ff374af2c08d384f9a24c939978562aa1d170
KellyShyno <kellyshyno@bk.ru>
2020-08-12 23:25:50 +0300

    rename

--- data/info/eng/customBash.sh
+++ data/info/eng/customBash.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #KellyShyno
 
-echo -e "\e[1;31mcustomShell\e[0m" "\e[1;36m- is the program for shell(bash)\e[0m";
+echo -e "\e[1;31mcustomBash\e[0m" "\e[1;36m- is the program for bash-shell\e[0m";
 echo -e "\e[1;36mcustomization in termux\e[0m";
 echo "";
 echo -e "\e[1;35mDescription:\e[0m";
--- data/info/ru/customBash.sh
+++ data/info/ru/customBash.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #KellyShyno
 
-echo -e "\e[1;31mcustomShell\e[0m" "\e[1;36m- программа для кастомизации\e[0m";
-echo -e "\e[1;36mshell'a(bash) в termux\e[0m";
+echo -e "\e[1;31mcustomBash\e[0m" "\e[1;36m- программа для кастомизации\e[0m";
+echo -e "\e[1;36mbash-шелла в termux\e[0m";
 echo "";
 echo -e "\e[1;35mОписание:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
