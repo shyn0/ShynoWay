d02f6fb3cdc2d7ccee0974e0be825ab70c746d03
KellyShyno <kellyshyno@bk.ru>
2020-10-02 12:56:42 +0300

    editValue

--- data/info/eng/customBash.sh
+++ data/info/eng/customBash.sh
@@ -6,7 +6,7 @@ echo -e "\e[1;36mcustomization in termux\e[0m";
 echo "";
 echo -e "\e[1;35mDescription:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
-echo -e "\e[1;36m6 colors\e[0m";
+echo -e "\e[1;36m8 colors\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
 echo -e "\e[1;36mchanging shell(bash)\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
--- data/info/ru/customBash.sh
+++ data/info/ru/customBash.sh
@@ -6,7 +6,7 @@ echo -e "\e[1;36mbash-шелла в termux\e[0m";
 echo "";
 echo -e "\e[1;35mОписание:\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
-echo -e "\e[1;36m6 цветов\e[0m";
+echo -e "\e[1;36m8 цветов\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
 echo -e "\e[1;36mизменение shell'a(bash)\e[0m";
 echo -en "\e[1;31m⋄ \e[0m";
